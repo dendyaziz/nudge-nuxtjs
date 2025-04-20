670ea63f875f26f658355afd040de9d95e8c79d0
Dendy <dendy.aziz@gmail.com>
2025-04-20 13:14:05 +0700

    feat: add debug button for Firestore connectivity testing and log Firebase config

--- pages/topics/[id].vue
+++ pages/topics/[id].vue
@@ -2,9 +2,10 @@
   <div class="max-w-md mx-auto p-4">
     <h1 class="text-2xl font-bold mb-4">Message to {{ topic?.fullName }}</h1>
     <div class="p-4 mb-4 border rounded bg-base-200">
-      <p>{{ topic?.softenedMessage }}</p>
+      <p v-if="!showRaw" v-html="previewMessage" class="whitespace-pre-line"></p>
+      <p v-else>{{ topic?.rawMessage }}</p>
     </div>
-    <button class="btn btn-link mb-4" @click="showRaw = !showRaw">
+    <button class="btn mb-4" @click="showRaw = !showRaw">
       {{ showRaw ? 'Hide' : 'Show' }} Original Message
     </button>
     <p v-if="showRaw" class="text-gray-500 mb-4">{{ topic?.rawMessage }}</p>
@@ -21,6 +22,31 @@ import { doc, getDoc } from 'firebase/firestore';
 const route = useRoute();
 const topic = ref<any>(null);
 const showRaw = ref(false);
+
+// Define the type for the soften data
+interface SoftenData {
+  soften_message: string;
+  suggestion: string;
+  disclaimer: string;
+}
+
+// make me computed value for the preview message send to the user
+const previewMessage = computed(() => {
+  if (!topic.value)
+    return ''
+
+  const softenData = JSON.parse(topic.value.softenData) as SoftenData
+
+  if (!softenData)
+    return ''
+
+  // Format the preview message, handling empty suggestion
+  if (softenData.suggestion) {
+    return `${softenData.soften_message}<br><br>${softenData.suggestion}`;
+  } else {
+    return softenData.soften_message;
+  }
+});
 
 onMounted(async () => {
   const firestore = useNuxtApp().$firestore as import('firebase/firestore').Firestore;
