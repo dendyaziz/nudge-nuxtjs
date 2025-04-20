1ce06ddfc1063438ea52eddfae5cbbd31e27af8d
Dendy <dendy.aziz@gmail.com>
2025-04-20 16:10:05 +0700

    feat: fetch and update message status from API and Firestore

--- pages/topics/[id].vue
+++ pages/topics/[id].vue
@@ -9,9 +9,23 @@
       </button>
     </div>
     <p v-if="showRaw" class="text-gray-500 mb-4">{{ topic?.rawMessage }}</p>
-    <div>
-      <p><strong>Status:</strong> {{ messageStatus }}</p>
-      <p v-if="messageStatusError" class="text-red-500 text-sm">{{ messageStatusError }}</p>
+    <div class="inline-flex gap-1 items-center">
+      <strong>Status:</strong>
+      <span v-if="messageStatus === 'DELIVERED'" class="relative group inline-flex items-center gap-1">
+        Terkirim
+        <div class="tooltip" data-tip="Penerima mungkin telah membaca pesan, tetapi mereka mematikan status 'Dibaca' pada WhatsApp di ponsel mereka.">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 -960 960 960"
+            class="w-5 fill-current text-base-content/70"
+          ><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+        </div>
+      </span>
+      <span v-else-if="messageStatus === 'SENT'">Terkirim ke server</span>
+      <span v-else-if="messageStatus === 'READ'">Dibaca</span>
+      <span v-else-if="messageStatus === 'PLAYED'">Diputar</span>
+      <span v-else>Tak Diketahui</span>
+      <p v-if="messageStatusError" class="text-red-500 text-sm">{{ messageStatusError }}</p>      <p v-if="messageStatusError" class="text-red-500 text-sm">{{ messageStatusError }}</p>
     </div>
   </div>
 </template>
