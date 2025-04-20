92d06d2125543a3a6e5b72bdb4799e2fd207fdbf
Dendy <dendy.aziz@gmail.com>
2025-04-20 18:33:14 +0700

    feat: enhance queue status display with tooltip and update timestamp handling

--- pages/topics/[id].vue
+++ pages/topics/[id].vue
@@ -9,13 +9,23 @@
         <div class="inline-flex gap-1 items-center">
           <strong>Status:</strong>
           <!-- Queue status -->
-          <span v-if="topic?.status === 'QUEUED'" class="badge badge-warning">
-            Dalam Antrian
-          </span>
+          <div v-if="topic?.status === 'QUEUED'" class="inline-flex items-center gap-1">
+            <span class="badge badge-warning">
+              Dalam Antrian
+            </span>
+
+            <div class="tooltip tooltip-accent" :data-tip="formatScheduledTime(topic.scheduledFor)">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 -960 960 960"
+                class="w-5 fill-current text-base-content/70"
+              ><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+            </div>
+          </div>
           <!-- Message status after sent -->
           <span v-else-if="messageStatus === 'DELIVERED'" class="badge badge-success relative group inline-flex items-center gap-1">
             Terkirim
-            <div v-if="false" class="tooltip" data-tip="Penerima mungkin telah membaca pesan, tetapi mereka mematikan status 'Dibaca' pada WhatsApp di ponsel mereka.">
+            <div v-if="false" class="tooltip tooltip-accent" data-tip="Penerima mungkin telah membaca pesan, tetapi mereka mematikan status 'Dibaca' pada WhatsApp di ponsel mereka.">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 -960 960 960"
@@ -75,9 +85,9 @@ function formatScheduledTime(timestamp: any): string {
 
   // Handle Firestore Timestamp
   let date;
-  if (timestamp.seconds) {
+  if (timestamp._seconds) {
     // Convert Firestore Timestamp to JavaScript Date
-    date = new Date(timestamp.seconds * 1000);
+    date = new Date(timestamp._seconds * 1000);
   } else if (timestamp.toDate) {
     // Handle Firestore Timestamp object with toDate method
     date = timestamp.toDate();
